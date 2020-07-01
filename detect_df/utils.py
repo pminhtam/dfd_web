01ea3ca05c9bb2cda84ccd89c2b207940625ff0d
Phạm Minh Tâm <pminhtamnb@gmail.com>
2020-07-01 10:09:20 +0700

    fix input image

--- detect_df/utils.py
+++ detect_df/utils.py
@@ -1,4 +1,5 @@
 from core.xception import xception
+from core.efficientnet import EfficientDual
 from facenet_pytorch import MTCNN
 import torch
 import numpy as np
@@ -9,9 +10,13 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 detector = MTCNN(device=device)
 margin = 0.2
-model = xception()
-model.load_state_dict(torch.load("../../model/xception/model_pytorch_4.pt",map_location=torch.device('cpu') ))
+# model = xception()
+# model.load_state_dict(torch.load("../../model/xception/model_pytorch_4.pt",map_location=torch.device('cpu') ))
+# model.eval()
+model = EfficientDual()
+model.load_state_dict(torch.load("../../model/model_dualpytorch3_1.pt", map_location=torch.device('cpu')))
 model.eval()
+# model.to(device)
 
 def get_model(checkpoint):
     global model
@@ -45,7 +50,7 @@ def detect_cnn(image):
     # face = image[y0:y1, x0:x1]
     # face = cv2.resize(face,(256,256))
     # face = transforms.ToTensor()(face)
-    # print(face)
+    # print(face_positions)
     # face = face.unsqueeze(0)
     # ressult = model.forward(face)
     # ressult = ressult.squeeze()
@@ -64,17 +69,38 @@ def detect_cnn(image):
             y1 = round(min(y + h + offsety, image.shape[0]))
             x0 = round(max(x - offsetx, 0))
             face = image[y0:y1, x0:x1]
-            face = cv2.resize(face,(256,256))
-            face = transforms.ToTensor()(face)
+            face = cv2.resize(face,(128,128))
+
+
+            f = np.fft.fft2(cv2.cvtColor(face,cv2.COLOR_RGB2GRAY))
+            fshift = np.fft.fftshift(f)
+            fshift += 1e-8
+
+            magnitude_spectrum = np.log(np.abs(fshift))
+            # img = np.concatenate([img,magnitude_spectrum],axis=2)
+            # img = np.transpose(img,(2,0,1))
+            magnitude_spectrum = cv2.resize(magnitude_spectrum, (128, 128))
+            magnitude_spectrum = np.array([magnitude_spectrum])
+            print(magnitude_spectrum.shape)
+            magnitude_spectrum = np.transpose(magnitude_spectrum, (1,2 , 0))
+            magnitude_spectrum = transforms.ToTensor()(magnitude_spectrum)
+
+            # face = transforms.ToTensor()(face)
+            face = transforms.Compose([transforms.ToTensor(),
+                                               transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                    std=[0.229, 0.224, 0.225])
+
+                                               ])(face)
             # print(face)
             face = face.unsqueeze(0)
-            ressult = model.forward(face)
+            magnitude_spectrum = magnitude_spectrum.unsqueeze(0)
+            ressult = model.forward(face,magnitude_spectrum)
             ressult = ressult.squeeze()
             ressult = ressult.detach().numpy()
             position.append(face_position)
             fake.append(ressult)
     except:
-        # print(face_positions)
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
         return None,None
     return position,fake
 # SIZE = [128,256,512]
